3320a1864f4a16dad88d8daa928948fca50a5b62
Golla Soundarya <156885278+Soundaryagolla09@users.noreply.github.com>
2024-10-17 06:07:21 +0530

    Add files via upload

---
+++ hangman_word_list.py
@@ -0,0 +1,18 @@
+# word_list.py
+
+words= [
+    "apple", "banana", "orange", "grapefruit", "watermelon", "strawberry", "blueberry", "raspberry",
+    "pineapple", "mango", "peach", "apricot", "plum", "cherry", "blackberry", "kiwi", "coconut",
+    "pear", "grape", "melon", "papaya", "fig", "date", "pomegranate", "lemon", "lime", "nectarine",
+    "mandarin", "persimmon", "quince", "dragonfruit", "jackfruit", "lychee", "tamarind", "durian",
+    "gooseberry", "currant", "elderberry", "cranberry", "passionfruit", "rambutan", "soursop",
+    "starfruit", "guava", "loquat", "mulberry", "cucumber", "tomato", "potato", "carrot", "onion",
+    "pepper", "broccoli", "cauliflower", "spinach", "lettuce", "cabbage", "zucchini", "pumpkin",
+    "squash", "okra", "radish", "turnip", "beetroot", "celery", "asparagus", "mushroom", "eggplant",
+    "parsley", "cilantro", "basil", "rosemary", "thyme", "sage", "oregano", "mint", "dill", "chives",
+    "tarragon", "fennel", "lavender", "bay", "coriander", "paprika", "cumin", "turmeric", "saffron",
+    "ginger", "garlic", "clove", "cinnamon", "nutmeg", "cardamom", "peppercorn", "vanilla", "mustard",
+    "sesame", "sunflower", "pumpkin", "chia", "flax", "poppy", "safflower", "hemp", "amaranth",
+    "quinoa", "millet", "barley", "oat", "rice", "wheat", "corn", "sorghum", "rye", "buckwheat",
+    "spelt", "teff", "kamut", "emmer", "freekeh", "triticale"
+]
